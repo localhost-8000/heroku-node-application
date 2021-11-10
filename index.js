5a7e2d42da934a52a469c45de1b95b84427ee879
Rahul Tiwari <rahultwr0005@gmail.com>
2021-11-10 23:19:55 +0530

    second commit

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
     res.send("hello guys");
 })
 
+app.get("/channel", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
