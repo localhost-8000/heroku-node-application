e234f4989e6a472025ccfe8d8dbd4a0056dc40fb
Rahul Tiwari <rahultwr0005@gmail.com>
2021-11-10 23:14:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hello guys");
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is started at port ${PORT}`);
+})
